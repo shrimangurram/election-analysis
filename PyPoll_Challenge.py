ad09cefee0a398f09dfa1a0e1095daedaafbe693
shrimangurram <sgurram@zoho.com>
2020-08-31 12:58:18 -0700

    commit files

---
+++ PyPoll_Challenge.py
@@ -0,0 +1,150 @@
+# The data we need to retrieve
+# 1. The total number of votes cast
+# 2. A complete list of counties where voting took place
+# 3. The total number of votes cast in each county
+# 4. A complete list of candidates who received votes
+# 5. The total number of votes each candidate won
+# 6. The winner of the election based on popular vote
+
+# Add our dependencies.
+import csv
+import os
+
+# Assign a variable to load a file from a path
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Assign a variable to save the file to a path
+file_to_save = os.path.join("analysis", "election_results.txt")
+
+# Initialize a total vote counter
+total_votes = 0
+
+# Counties and county votes.
+counties_name = []
+counties_votes = {}
+
+# Track the winning county
+winning_county = ""
+winning_county_count = 0
+winning_county_percentage = 0
+
+# Candidate options and candidate votes.
+candidate_options = []
+candidate_votes = {}
+
+# Track the winning candidate, vote count and percentage.
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read the header row.
+    headers = next(file_reader)
+
+    # Print each row in the CSV file
+    for row in file_reader:
+        # Add to the total vote count
+        total_votes += 1
+
+        # Get the county name and candidate name from each row
+        county_name = row[1]
+        candidate_name = row[2]
+
+        # If the county does not match any existing county, add the
+        # the county list
+        if county_name not in counties_name:
+            # Add the county name to the county list.
+            counties_name.append(county_name)
+            # And begin tracking that county voter count
+            counties_votes[county_name] = 0
+        
+        # Add a vote to that county count.
+        counties_votes[county_name] += 1
+
+        # If the candidate does not match any existing candidate, add the
+        # the candidate list
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+            # And begin tracking that candidate's voter count
+            candidate_votes[candidate_name] = 0
+        
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+# Save the results to our text file.
+with open(file_to_save, "w") as txt_file:
+    # Print the final vote count to the terminal
+    election_results = (
+        f"\nElection Results\n"
+        f"------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"------------------------\n\n"
+        f"County Votes:\n"
+    )
+    print(election_results, end="")
+
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+
+    for county in counties_votes:
+        # Retrieve vote count and percentage.
+        votes = counties_votes[county]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        county_results = (
+            f"{county}: {vote_percentage:.1f}% ({votes:,})\n"
+        )
+
+        # Print each counties voter count and percentage to the terminal.
+        print(county_results)
+        # Save the county results to our text file.
+        txt_file.write(county_results)
+        # Determine winning vote count, winning percentage, and winning candidate.
+        if (votes > winning_county_count) and (vote_percentage > winning_county_percentage) :
+            winning_county_count = votes
+            winning_county = county
+            winning_county_percentage = vote_percentage
+
+    # Print the winning counties results to the terminal.
+    winning_county_summary = (
+        f"\n\n-------------------------\n"
+        f"Largest County turnout: {winning_county}\n"
+        f"-------------------------\n"
+    )
+    print(winning_county_summary)
+    # Save the winning candidate's results to the text file.
+    txt_file.write(winning_county_summary)
+
+    for candidate in candidate_votes:
+        # Retrieve vote count and percentage.
+        votes = candidate_votes[candidate]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        candidate_results = (
+            f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n"
+        )
+
+        # Print each candidate's voter count and percentage to the terminal.
+        print(candidate_results)
+        # Save the candidate results to our text file.
+        txt_file.write(candidate_results)
+        # Determine winning vote count, winning percentage, and winning candidate.
+        if (votes > winning_count) and (vote_percentage > winning_percentage) :
+            winning_count = votes
+            winning_candidate = candidate
+            winning_percentage = vote_percentage
+
+    # Print the winning candidate's results to the terminal.
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}\n"
+        f"-------------------------\n"
+    )
+    
+    print(winning_candidate_summary)
+    # Save the winning candidate's results to the text file.
+    txt_file.write(winning_candidate_summary)
--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# election-analysis
+# Election Analysis
+
+## Project Overview
+A Colorado Board of Elections employee has given you the following tasks to complete the election audit of a recent local congressional election
+
+1. Calculate the total number of votes cast.
+2. Get a complete list of candidates who received votes.
+3. Calculate the total number of votes each candidate received.
+4. Calculate the percentage of votes each candidate won.
+5. Determine the winner of the election based on popular vote.
+
+## Resources
+- Data Source: election_results.csv
+- Software: Python 3.8.1, Visual Studio Code 1.41.1
+
+## Summary
+The analysis of the election show that:
+- There were 369,711 votes cast in the election.
+- The candidates were:
+  - Charles Casper Stockham
+  - Diana DeGette
+  - Raymon Anthony Doane
+- The candidate results were:
+  - Charles Casper Stockham received 23.0% of the vote and 85,213 number of votes
+  - Diana DeGette received 73.8% of the vote and 272,892 number of votes
+  - Raymon Anthony Doane received 3.1% of the vote and 11,606 number of votes
+- The winner of the election was:
+  -  Diana DeGette, who received 73.8% of the vote and 272,892 number of votes
+
+## Challenge Overview
+
+A Colorado Board of Elections employee  has given you the following tasks to complete the election audit of a recent congressional election
+
+1. Calculate the total number of votes cast.
+2. Get a complete list of counties where voting took place
+3. Calculate the total number of votes cast in each county
+4. Calculate the percentage of votes cast in each county
+5. Determine the county with the largest turnout
+
+
+## Challenge Summary
+The analysis of the election show that:
+- There were 369,711 votes cast in the election.
+- The counties where voting took place were:
+  - Jefferson
+  - Denver
+  - Arapahoe
+- The county voting statistics show that:
+  - In Jefferson county 10.5% of the total votes were cast and  number of votes cast were 38,855
+  - In Denver county 82.8% of the total votes were cast and  number of votes cast were 306,055
+  - In Arapahoe county 6.7% of the total votes were cast and  number of votes cast were 24,801
+- The county that had the largest turnout:
+  -  Denver county, where 306,055 number of votes were cast with a percentage of 82.8% of the total votes cast
